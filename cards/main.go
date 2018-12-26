76dc482dba24cddbe9e75c8ad1e1ce8cb64d4900
Luis Velez <luis.velez@simplesurance.de>
2018-12-26 20:31:32 +0100

    Add special cases for declarations outside of function scope

--- cards/main.go
+++ cards/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+//Possible to declare variables outside of the function scope without assigning a value to them
+var variableWithoutValue string
+
+//Possible to declare variables and assign value to them
+var variableWithtValue string = "value"
+
+//not possible to use short hand declaration syntax
+//shortHandDeclaredVariable := "shortHandValue"
+//produces: syntax error: non-declaration statement outside function body
+
 func main() {
 	//Long form declaration
 	// var card string = "Ace of Spades"
@@ -13,4 +23,6 @@ func main() {
 	card /*:*/ = "Five of Diamonds"
 
 	fmt.Println(card)
+	fmt.Println(variableWithoutValue)
+	fmt.Println(variableWithtValue)
 }
