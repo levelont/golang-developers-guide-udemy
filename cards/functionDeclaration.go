e0d82ade5f8dddecf9b6401897994c26333351dc
Luis Velez <luis.velez@simplesurance.de>
2018-12-26 22:18:39 +0100

    Rename functions to avoid name collisions

--- cards/functionDeclaration.go
+++ cards/functionDeclaration.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 // Explicitly indicate the return type value in a function declaration!
-func newCard() string {
+func createNewCard() string {
 	return "Five of Diamonds"
 }
 
-func createNewCard() {
+func createAndPrintNewCard() {
 	//Go inferes the type of the variable based on the return type set to the function
-	card := newCard() //type will be string
+	card := createNewCard() //type will be string
 
 	fmt.Println(card)
 }
